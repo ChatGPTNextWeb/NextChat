0e34f12a75c6338128de62caedc80489260ea78b
Ibrahim H. <bitsnaps@yahoo.fr>
2025-05-28 16:01:41 +0100

    support new claude-4 llms
    both sonnet & opus

--- app/constant.ts
+++ app/constant.ts
@@ -469,6 +469,7 @@ export const VISION_MODEL_REGEXES = [
   /gpt-4o/,
   /gpt-4\.1/,
   /claude-3/,
+  /claude-4/,
   /gemini-1\.5/,
   /gemini-exp/,
   /gemini-2\.0/,
@@ -568,6 +569,10 @@ const anthropicModels = [
   "claude-3-5-sonnet-latest",
   "claude-3-7-sonnet-20250219",
   "claude-3-7-sonnet-latest",
+  "claude-sonnet-4-0",
+  "claude-sonnet-4-20250514,
+  "claude-opus-4-0",
+  "claude-opus-4-20250514",
 ];
 
 const baiduModels = [
